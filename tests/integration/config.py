794d3035e591802b1624ad45e3c32d31af81f051
lrudenka <liuda.rudenka@oracle.com>
2023-03-02 16:12:06 -0600

    ODSC-37150. Add tests and actions
    - test 4

--- tests/integration/config.py
+++ tests/integration/config.py
@@ -5,10 +5,5 @@
 
 import os
 
-IT_DC_ACCESS_LOG_OCID = os.environ.get("IT_DC_ACCESS_LOG_OCID")  # ends with "yupl2a"
-IT_DC_LOG_GROUP_OCID = os.environ.get("IT_DC_LOG_GROUP_OCID")  # ends with "r7qoq"
-IT_DC_MODEL_DEPLOYMENT_OCID = os.environ.get("IT_DC_MODEL_DEPLOYMENT_OCID")  # ends with "x2y5q"
-IT_DC_PREDICT_LOG_OCID = os.environ.get("IT_DC_PREDICT_LOG_OCID")  # ends with "5rv5q"
-IT_JOBRUNPRE_OCID = os.environ.get("IT_JOBRUNPRE_OCID")  # ends with "rjk2a"
-IT_JOBS_IN_NETWORKS_LOG_OCID = os.environ.get("IT_JOBS_IN_NETWORKS_LOG_OCID")  # ends with "5srca"
 IT_NETWORKS_COMPARTMENT_OCID = os.environ.get("IT_NETWORKS_COMPARTMENT_OCID")  # ends with "kqteq"
+IT_TC_TEST_PROJECT_OCID = os.environ.get("IT_TC_TEST_PROJECT_OCID")  # ends with "wjafq"
