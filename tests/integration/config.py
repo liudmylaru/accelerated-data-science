aa2e03cefb3070018b9c2c0c8db0c930c04bbcf8
lrudenka <liuda.rudenka@oracle.com>
2023-03-14 17:18:58 -0500

    ODSC-37150. Add tests and actions
    - test 6

--- tests/integration/config.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-# Copyright (c) 2023 Oracle and/or its affiliates.
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
-
-import os
-
-IT_NETWORKS_COMPARTMENT_OCID = os.environ.get("IT_NETWORKS_COMPARTMENT_OCID")  # ends with "kqteq"
-IT_TC_TEST_PROJECT_OCID = os.environ.get("IT_TC_TEST_PROJECT_OCID")  # ends with "wjafq"
